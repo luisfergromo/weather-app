f5aef9cd7c104a8bf7b01b6658626248b00c7426
luisromo <ulab@iteso.mx>
2020-07-21 13:22:59 -0500

    1st Commit

---
+++ src/components/Titles.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Weather Finder</h2>
+        <p>Find out temperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
